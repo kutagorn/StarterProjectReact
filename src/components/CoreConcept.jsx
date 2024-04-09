947074c8a267497f8667be221fa5a4dd2b232332
Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
2024-04-09 12:51:48 +0100

    Making components in to separate Files for good project Structure

---
+++ src/components/CoreConcept.jsx
@@ -0,0 +1,11 @@
+export default function CoreConcept({image, title, description}) {
+    /*Used onject destructuring sytax to make the function shorter*/
+    return (
+      <li>
+        <img src={image} alt={title} />
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </li>
+    );
+  }
+  
