908402ea035e7db619fed6f1c2a2d31a387e9ec7
Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
2024-04-09 13:20:44 +0100

    The Special 'children' Prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { CORE_CONCEPTS } from "./data.js";
-import Header from './components/Header/Header.jsx';
+import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
-
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
   return (
@@ -11,10 +11,10 @@ function App() {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]}/> 
+            <CoreConcept {...CORE_CONCEPTS[0]} />
             {/*Shorter version. This pulls all the key-value pairs from the object 
             Prop names should be similar to the property names 
-            of the object that contains the data you wanna set*/ }
+            of the object that contains the data you wanna set*/}
             <CoreConcept
               title={CORE_CONCEPTS[1].title}
               description={CORE_CONCEPTS[1].description}
@@ -31,6 +31,13 @@ function App() {
               image={CORE_CONCEPTS[3].image}
             />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            {/* <TabButton>Components</TabButton> <---Children usage*/}
+            <TabButton label="Components" />
+          </menu>
         </section>
       </main>
     </div>
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,27 @@
+/*export default function TabButton(props){
+    using TabButton({children}) and using children below works too.
+    I wrote it like this to see how does .children works with props. 
+    This is called component composition
+
+return(
+    <li>
+        <button>{props.children}</button>
+    </li>
+);
+
+}
+*/
+
+export default function TabButton({ label }) {
+  return <li>{label}</li>;
+} /*
+if you just put a label without string decoupling you need to use it like this.
+export default function TabButton(props){
+    
+return(
+    <li>
+        <button>{props.label}</button>
+    </li>
+);
+
+} */
